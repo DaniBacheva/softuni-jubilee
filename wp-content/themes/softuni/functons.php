0031e7cb273d25523de0985c99a9b14cca518293
DaniBacheva <y.bacheva@abv.bg>
2024-11-29 08:52:13 +0200

    Initial commit, jubilee theme

---
+++ wp-content/themes/softuni/functons.php
@@ -0,0 +1,8 @@
+<?php
+
+add_theme_support ( 'post-thumbnails' );
+add_theme_support ( 'title-tag' );
+
+function softuni_enqueue_assets() {
+    
+}
---
+++ wp-content/themes/softuni/single.php
@@ -0,0 +1,349 @@
+<?php get_header(); ?>
+
+<section id="about" class="padding-medium mt-xl-5">
+    <div class="container">
+      <div class="row align-items-center mt-xl-5">
+        <div class="offset-md-1 col-md-5">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/about-img.jpg" alt="img" class="img-fluid rounded-circle">
+        </div>
+        <div class="col-md-5 mt-5 mt-md-0">
+          <div class="mb-3">
+            <p class="text-secondary ">Learn more about us</p>
+            <h2 class="display-6 fw-semibold">About Us</h2>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem molestiae nam commodi dolore vitae?
+            Numquam minima cum asperiores deleniti possimus provident, officia itaque esse eius, delectus incidunt
+            laudantium adipisci laboriosam!</p>
+          <div class="d-flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="22px" height="22px">
+              <use href="#tick-circle" class="text-secondary" />
+            </svg>
+            <p class="ps-4">Engage with a worldwide community of inquisitive and imaginative individuals.</p>
+          </div>
+          <div class="d-flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="22px" height="22px">
+              <use href="#tick-circle" class="text-secondary" />
+            </svg>
+            <p class="ps-4">Learn a skill of your choice from the experts around the world from various industries</p>
+          </div>
+          <div class="d-flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="22px" height="22px">
+              <use href="#tick-circle" class="text-secondary" />
+            </svg>
+            <p class="ps-4">Learn a skill of your choice from the experts around the world from various industries</p>
+          </div>
+          <a href="about.html" class="btn btn-primary px-5 py-3 mt-4">Learn more</a>
+
+
+        </div>
+      </div>
+    </div>
+</section>
+
+<section id="courses" class="padding-medium">
+    <div class="container">
+      <div class="text-center mb-5">
+        <p class="text-secondary">Some of our most popular online courses</p>
+        <h2 class="display-6 fw-semibold">Explore Inspiring Online Courses</h2>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-6 col-lg-4 col-xl-3 mb-5">
+          <div class="z-1 position-absolute m-4">
+            <span class="badge text-white bg-secondary">New</span>
+          </div>
+          <div class="card rounded-4 border-0 shadow-sm p-3 position-relative">
+            <a href="courses-details.html"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/item1.jpg" class="img-fluid rounded-3" alt="image"></a>
+            <div class="card-body p-0">
+
+              <div class="d-flex justify-content-between my-3">
+                <p class="text-black-50 fw-bold text-uppercase m-0">Digital Marketing</p>
+                <div class="d-flex align-items-center">
+                  <svg width="20" height="20">
+                    <use xlink:href="#clock" class="text-black-50"></use>
+                  </svg>
+                  <p class="text-black-50 fw-bold text-uppercase m-0">1h 50m</p>
+                </div>
+              </div>
+
+              <a href="courses-details.html">
+                <h5 class="course-title py-2 m-0">Mastering the Art of Digital Communication</h5>
+              </a>
+
+              <div class="card-text">
+                <span class="rating d-flex align-items-center mt-3">
+                  <p class="text-muted fw-semibold m-0 me-2">By: James Willam</p>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                </span>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3 mb-5">
+          <div class="z-1 position-absolute m-4">
+            <span class="badge text-white bg-secondary">New</span>
+          </div>
+          <div class="card rounded-4 border-0 shadow-sm p-3 position-relative">
+            <a href="courses-details.html"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/item2.jpg" class="img-fluid rounded-3" alt="image"></a>
+            <div class="card-body p-0">
+
+              <div class="d-flex justify-content-between my-3">
+                <p class="text-black-50 fw-bold text-uppercase m-0">Digital Marketing</p>
+                <div class="d-flex align-items-center">
+                  <svg width="20" height="20">
+                    <use xlink:href="#clock" class="text-black-50"></use>
+                  </svg>
+                  <p class="text-black-50 fw-bold text-uppercase m-0">1h 50m</p>
+                </div>
+              </div>
+
+              <a href="courses-details.html">
+                <h5 class="course-title py-2 m-0">Web Wizardry 101: Mastering the Internet</h5>
+              </a>
+
+              <div class="card-text">
+                <span class="rating d-flex align-items-center mt-3">
+                  <p class="text-muted fw-semibold m-0 me-2">By: James Willam</p>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                </span>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3 mb-5">
+          <!-- <div class="z-1 position-absolute m-4">
+            <span class="badge text-white bg-secondary">New</span>
+          </div> -->
+          <div class="card rounded-4 border-0 shadow-sm p-3 position-relative">
+            <a href="courses-details.html"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/item3.jpg" class="img-fluid rounded-3" alt="image"></a>
+            <div class="card-body p-0">
+
+              <div class="d-flex justify-content-between my-3">
+                <p class="text-black-50 fw-bold text-uppercase m-0">Digital Marketing</p>
+                <div class="d-flex align-items-center">
+                  <svg width="20" height="20">
+                    <use xlink:href="#clock" class="text-black-50"></use>
+                  </svg>
+                  <p class="text-black-50 fw-bold text-uppercase m-0">1h 50m</p>
+                </div>
+              </div>
+
+              <a href="courses-details.html">
+                <h5 class="course-title py-2 m-0">E-Learning Essentials: A Comprehensive Guide</h5>
+              </a>
+
+              <div class="card-text">
+                <span class="rating d-flex align-items-center mt-3">
+                  <p class="text-muted fw-semibold m-0 me-2">By: James Willam</p>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                </span>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3 mb-5">
+          <div class="z-1 position-absolute m-4">
+            <span class="badge text-white bg-secondary">Sale</span>
+          </div>
+          <div class="card rounded-4 border-0 shadow-sm p-3 position-relative">
+            <a href="courses-details.html"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/item4.jpg" class="img-fluid rounded-3" alt="image"></a>
+            <div class="card-body p-0">
+
+              <div class="d-flex justify-content-between my-3">
+                <p class="text-black-50 fw-bold text-uppercase m-0">Digital Marketing</p>
+                <div class="d-flex align-items-center">
+                  <svg width="20" height="20">
+                    <use xlink:href="#clock" class="text-black-50"></use>
+                  </svg>
+                  <p class="text-black-50 fw-bold text-uppercase m-0">1h 50m</p>
+                </div>
+              </div>
+
+              <a href="courses-details.html">
+                <h5 class="course-title py-2 m-0">CyberClass 101: A Guide to Online Education</h5>
+              </a>
+
+              <div class="card-text">
+                <span class="rating d-flex align-items-center mt-3">
+                  <p class="text-muted fw-semibold m-0 me-2">By: James Willam</p>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                </span>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3 mb-5">
+          <!-- <div class="z-1 position-absolute m-4">
+            <span class="badge text-white bg-secondary">New</span>
+          </div> -->
+          <div class="card rounded-4 border-0 shadow-sm p-3 position-relative">
+            <a href="courses-details.html"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/item5.jpg" class="img-fluid rounded-3" alt="image"></a>
+            <div class="card-body p-0">
+
+              <div class="d-flex justify-content-between my-3">
+                <p class="text-black-50 fw-bold text-uppercase m-0">Digital Marketing</p>
+                <div class="d-flex align-items-center">
+                  <svg width="20" height="20">
+                    <use xlink:href="#clock" class="text-black-50"></use>
+                  </svg>
+                  <p class="text-black-50 fw-bold text-uppercase m-0">1h 50m</p>
+                </div>
+              </div>
+
+              <a href="courses-details.html">
+                <h5 class="course-title py-2 m-0">Mastering the Art of Digital Communication</h5>
+              </a>
+
+              <div class="card-text">
+                <span class="rating d-flex align-items-center mt-3">
+                  <p class="text-muted fw-semibold m-0 me-2">By: James Willam</p>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                </span>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3 mb-5">
+          <div class="z-1 position-absolute m-4">
+            <span class="badge text-white bg-secondary">New</span>
+          </div>
+          <div class="card rounded-4 border-0 shadow-sm p-3 position-relative">
+            <a href="courses-details.html"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/item6.jpg" class="img-fluid rounded-3" alt="image"></a>
+            <div class="card-body p-0">
+
+              <div class="d-flex justify-content-between my-3">
+                <p class="text-black-50 fw-bold text-uppercase m-0">Digital Marketing</p>
+                <div class="d-flex align-items-center">
+                  <svg width="20" height="20">
+                    <use xlink:href="#clock" class="text-black-50"></use>
+                  </svg>
+                  <p class="text-black-50 fw-bold text-uppercase m-0">1h 50m</p>
+                </div>
+              </div>
+
+              <a href="courses-details.html">
+                <h5 class="course-title py-2 m-0">Web Wizardry 101: Mastering the Internet</h5>
+              </a>
+
+              <div class="card-text">
+                <span class="rating d-flex align-items-center mt-3">
+                  <p class="text-muted fw-semibold m-0 me-2">By: James Willam</p>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                </span>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3 mb-5">
+          <!-- <div class="z-1 position-absolute m-4">
+            <span class="badge text-white bg-secondary">New</span>
+          </div> -->
+          <div class="card rounded-4 border-0 shadow-sm p-3 position-relative">
+            <a href="courses-details.html"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/item7.jpg" class="img-fluid rounded-3" alt="image"></a>
+            <div class="card-body p-0">
+
+              <div class="d-flex justify-content-between my-3">
+                <p class="text-black-50 fw-bold text-uppercase m-0">Digital Marketing</p>
+                <div class="d-flex align-items-center">
+                  <svg width="20" height="20">
+                    <use xlink:href="#clock" class="text-black-50"></use>
+                  </svg>
+                  <p class="text-black-50 fw-bold text-uppercase m-0">1h 50m</p>
+                </div>
+              </div>
+
+              <a href="courses-details.html">
+                <h5 class="course-title py-2 m-0">E-Learning Essentials: A Comprehensive Guide</h5>
+              </a>
+
+              <div class="card-text">
+                <span class="rating d-flex align-items-center mt-3">
+                  <p class="text-muted fw-semibold m-0 me-2">By: James Willam</p>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                </span>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3 mb-5">
+          <div class="z-1 position-absolute m-4">
+            <span class="badge text-white bg-secondary">Sale</span>
+          </div>
+          <div class="card rounded-4 border-0 shadow-sm p-3 position-relative">
+            <a href="courses-details.html"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/item8.jpg" class="img-fluid rounded-3" alt="image"></a>
+            <div class="card-body p-0">
+
+              <div class="d-flex justify-content-between my-3">
+                <p class="text-black-50 fw-bold text-uppercase m-0">Digital Marketing</p>
+                <div class="d-flex align-items-center">
+                  <svg width="20" height="20">
+                    <use xlink:href="#clock" class="text-black-50"></use>
+                  </svg>
+                  <p class="text-black-50 fw-bold text-uppercase m-0">1h 50m</p>
+                </div>
+              </div>
+
+              <a href="courses-details.html">
+                <h5 class="course-title py-2 m-0">CyberClass 101: A Guide to Online Education</h5>
+              </a>
+
+              <div class="card-text">
+                <span class="rating d-flex align-items-center mt-3">
+                  <p class="text-muted fw-semibold m-0 me-2">By: James Willam</p>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                  <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
+                </span>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="text-center mt-4">
+        <a href="courses.html" class="btn btn-primary px-5 py-3">View all courses</a>
+      </div>
+
+    </div>
+</section>
+
+
+<?php get_footer(); ?>
