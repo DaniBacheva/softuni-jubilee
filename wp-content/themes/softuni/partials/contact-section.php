4e528a5fdf4f46c30e40964c03dd3498f4b02145
DaniBacheva <y.bacheva@abv.bg>
2024-12-11 15:08:50 +0200

    Contact section with ACF

--- wp-content/themes/softuni/partials/contact-section.php
+++ wp-content/themes/softuni/partials/contact-section.php
@@ -1,19 +1,37 @@
+<?php
+//var_dump( $args );
+?>
+
 <section id="register">
     <div class="container padding-medium">
       <div class="row align-items-center">
         <div class="offset-md-1 col-md-5 ">
           <div>
-            <h2 class="display-6 fw-semibold">Subscribe and get 20% OFF on your first online course </h2>
-            <p class="text-secondary ">Sign Up for our newsletter and never miss any offers</p>
+          <?php if ( ! empty( $args['subscribe'] ) ) : ?>
+            <h2 class="display-6 fw-semibold"><?php echo esc_attr( $args['subscribe'] ); ?></h2>
+            <?php endif; ?>   
+      
+            <?php if ( ! empty( $args['sign'] ) ) : ?>
+            <p class="text-secondary "><?php echo esc_attr( $args['sign'] ); ?></p>
+            <?php endif; ?> 
           </div>
         </div>
         <div class="col-md-5">
           <form id="form">
-            <input type="email" name="email" placeholder="Write Your Email Address*"
+               <input type="email" name="email" placeholder="Write Your Email Address*"
               class="form-control bg-white border-0 rounded-4 shadow-none rounded-pill text-center px-4 py-3 w-100 mb-4">
-            <div class="d-grid">
-              <button class="btn btn-primary px-5 py-3"> Get Started now</button>
-            </div>
+		
+
+              <?php if ( ! empty( $args['cta_text'] ) && $args['cta_url'] ) : ?>
+					
+
+                <div class="btn-wrap">
+						<a href="<?php echo esc_url( $args['cta_url'] ); ?>" class="btn btn-primary px-5 py-3 mt-4"><?php echo esc_attr( $args['cta_text'] ); ?></a>
+					</div>
+						<!-- <button class="btn btn-primary px-5 py-3"><a  href="<?php echo esc_url( $args['cta_url'] ); ?>" ><?php echo esc_attr( $args['cta_text'] ); ?></a></button>
+					</div> -->
+				<?php endif; ?>
+
           </form>
         </div>
 
